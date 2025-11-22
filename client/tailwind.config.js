e6159ce9afe154d66fefd5849d2680d6b39d6a1a
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-11-22 15:56:58 -0500

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}" // make sure it covers all your files
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
