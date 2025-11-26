4ab18a377a74a1932f417e70f07a933b3b8c8272
Abnet Mekonen <abnetmekonen32@gmail.com>
2025-11-26 07:15:51 -0500

    Update CustomerDetail.tsx

--- client/src/components/admin/CustomerDetail.tsx
+++ client/src/components/admin/CustomerDetail.tsx
@@ -222,9 +222,9 @@ const CustomerDetail: React.FC<CustomerDetailProps> = ({
                           <span
                             className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
                               order.status === "Completed"
-                                ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200"
+                                ? "bg-green-100 text-green-600 dark:bg-green-600 dark:text-green-200"
                                 : order.status === "In Progress"
-                                ? "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-200"
+                                ? "bg-yellow-100 text-red-600 dark:bg-red-600 dark:text-yellow-200"
                                 : "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200"
                             }`}
                           >
