3d632ef83036a7d8f36a478f626d3bb76e43ee60
Abnet Mekonen Gosaye <abnetmekonen32@gmail.com>
2026-04-06 19:04:29 +0300

    style:service page

--- components/Services.tsx
+++ components/Services.tsx
@@ -117,7 +117,7 @@ const Services: React.FC<ServicesProps> = ({ onLearnMore }) => {
       title: "Brake Repair & Service",
       image: three.src,
       prompt:
-        "My brakes are making",
+        "My brakes are makin",
       shortDescription:
         "Comprehensive brake inspections and repairs for safety.",
       extendedDescription:
